66eaf6148b96f9feb8bd754ae1da21abe1518c79
celia garcia fernandez <cgarcia@zeta31.aulas.gsyc.es>
2015-01-09 14:19:22 +0100

    INVITE implementado

--- uaclient.py
+++ uaclient.py
@@ -57,47 +57,99 @@ if __name__ == "__main__":
     dicc = small.get_tags()
          
         
-    # REGISTER
+# REGISTER
     if METODO == 'REGISTER':
         username = dicc['account_username']
         port = dicc['uaserver_puerto']
         linea = METODO + ' sip:' + username + ':' + port + ' SIP/2.0\r\n\r\n'
         linea += 'Expires: ' + str(OPTION) + '\r\n\r\n'
         
-        # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        my_socket.connect((dicc['regproxy_ip'], int(dicc['regproxy_puerto'])))
-        
-        print "Enviando: " + linea
-        my_socket.send(linea + '\r\n')
-        
-        data = my_socket.recv(1024)
-        print 'Recibido -- ', data
+        try:
+            # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            my_socket.connect((dicc['regproxy_ip'], int(dicc['regproxy_puerto'])))
             
-    # INVITE
+            print "Enviando: " + linea
+            my_socket.send(linea + '\r\n')
+            
+            data = my_socket.recv(1024)
+            print 'Recibido -- ', data
+            
+            print "Terminando socket..."
+            my_socket.close()
+            print "Fin."
+            
+        except socket.gaierror:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+        except socket.error:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+                
+# INVITE
     elif METODO == 'INVITE':    
         linea = METODO + ' sip:' + OPTION + ' SIP/2.0\r\n'
         linea += 'Content-Type: application/sdp' + '\r\n\r\n'
         linea += 'v=0\r\n'
-        linea += 'o=' + dicc['account_username'] + dicc['uaserver_ip'] + '\r\n'
+        linea += 'o=' + dicc['account_username'] + ' ' + dicc['uaserver_ip'] + '\r\n'
         linea += 's=ptavi-pfinal\r\n'
         linea += 't=0\r\n'
         linea += 'm=audio ' + dicc['rtpaudio_puerto'] + ' RTP\r\n\r\n'
         
-        # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        my_socket.connect((dicc['regproxy_ip'], int(dicc['regproxy_puerto'])))
-        
-        print "Enviando: " + linea
-        my_socket.send(linea + '\r\n')
-        
-        data = my_socket.recv(1024)
-        print 'Recibido -- ', data
+        try:
+            # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            my_socket.connect((dicc['regproxy_ip'], int(dicc['regproxy_puerto'])))
+            
+            print "Enviando: " + linea
+            my_socket.send(linea + '\r\n')
+            
+            data = my_socket.recv(1024)
+            print 'Recibido -- ', data
+            
+            print "Terminando socket..."
+            my_socket.close()
+            print "Fin."
+            
+        except socket.gaierror:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+        except socket.error:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+            
+# BYE            
+    elif METODO == 'BYE':
+        linea = METODO + ' sip:' + OPTION + ' SIP/2.0\r\n'
     
-    print "Terminando socket..."
-    my_socket.close()
-    print "Fin."
+        try:
+            # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            my_socket.connect((dicc['regproxy_ip'], int(dicc['regproxy_puerto'])))
+            
+            print "Enviando: " + linea
+            my_socket.send(linea + '\r\n')
+            
+            data = my_socket.recv(1024)
+            print 'Recibido -- ', data
+            
+            if data == 'SIP/2.0 200 OK':
+                print "Terminando socket..."
+                my_socket.close()
+                print "Fin."
+            
+        except socket.gaierror:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+        except socket.error:
+            com = 'Error: No server listening at '
+            sys.exit(com + dicc['regproxy_ip'] + ' port ' + dicc['regproxy_puerto'])
+    
+    
+    
+    
     
     
